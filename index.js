fa0dfb0c9a5cdc6b64203cea5df29d7dc4dc25d0
MatyasGulyas <guma.prog@gmail.com>
2022-05-29 07:45:47 +0200

    init

---
+++ index.js
@@ -0,0 +1,156 @@
+const { response } = require('express');
+const express = require('express');
+const fs = require('fs');
+const nodemailer = require('nodemailer');
+const app = express();
+const Datastore = require('nedb');
+
+app.listen(3000, () => console.log('listening at 3000'));
+app.use(express.static('public'));
+app.use(express.json({ limit: '1mb' }));
+
+const db = new Datastore('database.db');
+db.loadDatabase();
+
+db.count({}, function (err, count) {
+    if (count == 0) {
+        // ha a db üres, akkor fusson le és töltse fel a db-t
+        let rawdata = fs.readFileSync('data.json');
+        let nftData = JSON.parse(rawdata);
+        db.insert(nftData);
+    } else {
+        //ha a bd-be benne vannak az elemek, akkor fusson le
+        //a post method ami bekéri a kliens oldalról az új módosított objektumot és ezzel felülírja a db-t
+        console.log("van adat a DB-ben")
+    }
+});
+
+
+//Ez elküldi az adatbázis tartalmát a kliens oldalnak
+// app.get('/api', (request, response) => {
+//     db.find({}, (err, data) => {
+//         response.json(data)
+//     });
+// });
+
+
+app.post('/api', (request, response) => {
+    console.log('I got a request!');
+    let data = request.body;
+    let timestamp = Date.now();
+    data.timestamp = timestamp;
+    console.log(data);
+
+    //Ha a bonusUsed értéke false, akkor mehetünk csak tovább. Ha true, akkor visszadobja --> pipa
+
+    db.find({ "nftSecret": data.userSecret }, function (err, docs) {
+        let used = "";
+        if (docs.length != 0) {
+            used = docs[0].bonusUsed;
+            //meg kell nézni, hogy a data.userSecret-hez van-e az adatbázisban adat. Ha van, akkor vissza kell adni a response-ban a képet és társait --> pipa
+            if (used == false) {
+                console.log(docs[0].src)
+                response.json({
+                    status: "success",
+                    nftId: docs[0].nftId,
+                    nftSrc: docs[0].src
+                })
+            } else {
+                console.log(`Bonus is used: ${err}`);
+                response.json({
+                    status: "bonusUsed"
+                })
+            }
+
+        } else {
+            console.log(`Bonus don't matches: ${err}`);
+            response.json({
+                status: "unsuccesful"
+            })
+        }
+
+    });
+
+});
+
+app.post('/datas', (request, response) => {
+    console.log('I got a new request!');
+    data = request.body;
+    let timestamp = Date.now();
+    data.timestamp = timestamp;
+    console.log(data);
+
+    //Emailt kell küldeni + át kell állítani a bonusUsed értékét true-ra --> Így tudjuk, hogy az már fel lett használva!!
+
+    let transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'matyas.gulyas92@gmail.com',
+            pass: 'G19920601m'
+        }
+    });
+
+    let mailOptions = {
+        from: 'matyas.gulyas92@gmail.com',
+        to: 'guma.prog@gmail.com',
+        subject: `Incoming message from ${data.firstName}`,
+        text: `${data.firstName} ${data.familyName} has activated the secret code to her/his NFT
+        Necessary datas to accomplish the shipping: 
+        mail: ${data.userMail}
+        Details for Post: ${data.address}`
+    };
+
+    transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Datas sent to me: ' + info.response);
+            db.update({ nftSecret: data.secret }, { $set: { bonusUsed: data.bonusUsed } }, {}, function (err, numReplaced) {
+                // numReplaced = 3
+                // Field 'system' on Mars, Earth, Jupiter now has value 'solar system'
+            });
+            //Response error is legyen külön kezelve?
+            sendResponse();
+            response.json({
+                status: "success"
+            })
+        }
+    });
+
+});
+
+function sendResponse() {
+
+    let transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'matyas.gulyas92@gmail.com',
+            pass: 'G19920601m'
+        }
+    });
+
+    let mailOptions = {
+        from: 'matyas.gulyas92@gmail.com',
+        to: data.userMail,
+        subject: `Thank You for Your Purchase!! - Your NFT is on the way ;)`,
+        text: `Dear ${data.firstName} ${data.familyName}!
+
+                We have recieved your datas to accomplish the sending.
+                Your NFT would be send to you as soon as possible
+                We will inform you if we know the estimated delivery time depending on your location.
+                
+                Sincirely, 
+                Papp Lajos Máté `
+    };
+
+    transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email sent to user: ' + info.response);
+        }
+    });
+
+}
+
+
---
+++ public/app.js
@@ -0,0 +1,157 @@
+const inputContainer = document.querySelector('.inputList');
+const imgContainer = document.querySelector('.imgList');
+const submitSecretBtn = document.querySelector('#submitSecret');
+const inputSecret = document.querySelector('#secret');
+const wrongSecret = document.querySelector('.wrongSecret');
+const usedSecret = document.querySelector('.usedSecret');
+const responseMessage = document.querySelector('.response');
+const responseFailed = document.querySelector('.responseFailed');
+const submitFormBtn = document.querySelector('#submit');
+let userSecret = "";
+
+imgContainer.style.display = "none";
+inputContainer.style.display = "none";
+wrongSecret.style.display = "none";
+usedSecret.style.display = "none";
+responseMessage.style.display = "none";
+responseFailed.style.display = "none";
+
+async function submitSecret() {
+    userSecret = document.getElementById("secret").value;
+    const userSecretDb = { "userSecret": userSecret };
+
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify(userSecretDb)
+    };
+
+    const response = await fetch('/api', options);
+    const data = await response.json();
+    console.log(data);
+
+    if (data.status === "success") {
+
+        submitSecretBtn.style.display = "none";
+        inputSecret.style.display = "none";
+        usedSecret.style.display = "none";
+        wrongSecret.style.display = "none";
+        inputContainer.style.display = "flex";
+        imgContainer.style.display = "flex";
+
+
+        let imgSegment = `
+                <h2>Your NFT: Nr.${data.nftId}</h2>
+                <img src="${data.nftSrc}" id="${data.nftId}" >
+                <p>Wrong NFT? Contact us: xy@zmail.com</p>`;
+
+        imgContainer.innerHTML = imgSegment;
+
+    } else if (data.status === "bonusUsed") {
+        // ide majd jöhet a UI módosítás, felugró ablakkal... visible unvisible switch... HTML-ben megcsinálni és itt csak switchelni a konténert
+        usedSecret.style.display = "inline";
+        usedSecret.childNodes[1].style.color = "red";
+        wrongSecret.style.display = "none";
+    } else {
+        wrongSecret.style.display = "inline";
+        wrongSecret.childNodes[1].style.color = "red";
+        usedSecret.style.display = "none";
+    }
+
+
+    console.log(userSecret);
+}
+
+
+async function submitData() {
+    const userFirstName = document.querySelector('#firstName').value;
+    const userFamilyName = document.querySelector('#familyName').value;
+    const userMail = document.querySelector('#mail').value;
+    const userAddress = [document.querySelector('#country').value, document.querySelector('#zipCode').value, document.querySelector('#city').value, document.querySelector('#address').value]
+
+    if (userFirstName && userFamilyName && userMail && userAddress != "") {
+
+        let validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+        if (userMail.match(validRegex)) {
+
+            console.log("Valid email address!");
+
+            function handleForm(event) { event.preventDefault(); }
+            inputContainer.addEventListener('submit', handleForm);
+
+            submitFormBtn.style.visibility = "hidden";
+
+            const userData = {
+                "firstName": userFirstName,
+                "familyName": userFamilyName,
+                "userMail": userMail,
+                "address": userAddress,
+                "bonusUsed": true,
+                "secret": userSecret
+            }
+
+            const options = {
+                method: 'POST',
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify(userData)
+            };
+
+            const response = await fetch('/datas', options);
+            const data = await response.json();
+            console.log(data);
+
+
+            if (data.status === "success") {
+                //Ezt csak akkor kapom, ha a BackEnd már el is küldte az adatokat mailben
+
+                responseMessage.style.display = "inline";
+                inputContainer.style.display = "none";
+                imgContainer.style.display = "none";
+
+            } else {
+
+                responseFailed.style.display = "inline";
+                responseFailed.childNodes[1].style.color = "red";
+                inputContainer.style.display = "none";
+                imgContainer.style.display = "none";
+
+            }
+
+        } else {
+            console.log("Invalid Email...")
+        };
+
+    } else {
+        console.log("Missing Data from User!");
+    }
+
+}
+
+        // async function getData() {
+        //     const response = await fetch('/api');
+        //     const data = await response.json();
+        //     console.log(data);
+
+        //     async function renderImages() {
+        //         let html = '';
+        //         data.forEach(image => {
+        //             let htmlSegment = `<img src="${image.src}" id="${image.nftId}" >`;
+        //             html += htmlSegment;
+        //         });
+
+        //         let container = document.querySelector('.imgList');
+        //         container.innerHTML = html;
+        //     }
+
+        //     renderImages();
+
+        //     console.log(data[0].nftId);
+
+        // }
+
+        // getData();
